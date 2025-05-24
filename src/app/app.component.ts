d9c98eecb2d7fd3b1270a6b065008e4bfd608aaf
Fabio Rangel <fabiorangeldiasfrd@gmail.com>
2025-05-24 00:49:26 -0300

    feat: integrate footer component into main application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { HeaderComponent } from "./components/header/header.component";
 import { HeroComponent } from "./components/hero/hero.component";
 import { PlanosSectionComponent } from "./components/planos-section/planos-section.component";
 import { ContactSectionComponent } from "./components/contact-section/contact-section.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, HeroComponent, PlanosSectionComponent, ContactSectionComponent],
+  imports: [RouterOutlet, HeaderComponent, HeroComponent, PlanosSectionComponent, ContactSectionComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

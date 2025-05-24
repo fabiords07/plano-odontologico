adb244f5d4228f6e552539d267f2e8fc924c94fb
Fabio Rangel <fabiorangeldiasfrd@gmail.com>
2025-05-23 22:14:33 -0300

    feat: integrate contact section into main application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { HeroComponent } from "./components/hero/hero.component";
 import { PlanosSectionComponent } from "./components/planos-section/planos-section.component";
+import { ContactSectionComponent } from "./components/contact-section/contact-section.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, HeroComponent, PlanosSectionComponent],
+  imports: [RouterOutlet, HeaderComponent, HeroComponent, PlanosSectionComponent, ContactSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
